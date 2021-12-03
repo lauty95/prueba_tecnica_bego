a0cd03f2a4f169f72c0023b9b5a2ae3798453920
lauty95 <lautaroJ95@gmail.com>
2021-12-03 17:58:18 -0300

    falta el grad

--- src/components/Graphic.jsx
+++ src/components/Graphic.jsx
@@ -5,18 +5,30 @@ import { Line } from 'react-chartjs-2'
 function Graphic() {
 
     const data = (canvas) => {
-        const ctx = canvas.getContext("2d");
-        const gradient = ctx.createLinearGradient(0, 0, 0, 200);
-        gradient.addColorStop(0, '#594200');
-        gradient.addColorStop(0.6, '#040B11');
-        gradient.addColorStop(1, '#10161C');
+
+        const degrade = ({ gradient, a, b, c, d }) => {
+            const ctx = canvas.getContext("2d");
+            gradient = ctx.createLinearGradient(a, b, c, d);
+            gradient.addColorStop(0, '#594200');
+            gradient.addColorStop(0.6, '#040B11');
+            gradient.addColorStop(1, '#10161C');
+            return gradient
+        }
 
         return {
             labels: ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov'],
             datasets: [{
                 data: [12, 15, 10, 18, 15, 20],
                 fill: 'start',
-                backgroundColor: gradient,
+                backgroundColor: [
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 100 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 80 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 120 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 50 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 100 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 180 }),
+                    degrade({ gradient: '', a: 0, b: 0, c: 0, d: 100 }),
+                ],
                 borderColor: '#FFBE00',
                 borderWidth: 1
             }]
@@ -42,22 +54,34 @@ function Graphic() {
                     width={200}
                     options={
                         {
+                            elements: {
+                                point: {
+                                    radius: 0
+                                }
+                            },
                             legend: {
                                 display: false
                             },
                             maintainAspectRatio: false,
                             scales: {
-                                y: {
-                                    beginAtZero: true
-                                },
                                 yAxes: [
                                     {
+                                        gridLines: {
+                                            drawBorder: true,
+                                            display: false
+                                        },
                                         ticks: {
                                             beginAtZero: true,
                                             display: false
                                         }
                                     }
-                                ]
+                                ],
+                                xAxes: [{
+                                    gridLines: {
+                                        drawBorder: true,
+                                        display: false
+                                    }
+                                }]
                             }
                         }
                     }
